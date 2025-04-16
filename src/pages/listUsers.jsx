3f00d603119d9bf0d1d05cad49831f665db521c7
Joqueiroz <joaoqueiroz964@gmail.com>
2025-04-16 16:13:10 -0300

    aula-16/04

--- src/pages/listUsers.jsx
+++ src/pages/listUsers.jsx
@@ -1,90 +1,138 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 // Imports para criação de tabela
-import Table from '@mui/material/Table';
-import TableContainer from '@mui/material/TableContainer';
+import Table from "@mui/material/Table";
+import TableContainer from "@mui/material/TableContainer";
 // TableHead é onde colocamos os titulos
-import TableHead from '@mui/material/TableHead';
+import TableHead from "@mui/material/TableHead";
 // TableBody é onde colocamos o conteúdo
-import TableBody from '@mui/material/TableBody';
-import TableRow from '@mui/material/TableRow';
-import TableCell from '@mui/material/TableCell';
-import Paper from '@mui/material/Paper';
-import api from '../axios/axios'
-import { Button } from '@mui/material';
-import { Link, useNavigate } from 'react-router-dom';
-import { Logout } from '@mui/icons-material';
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import Paper from "@mui/material/Paper";
+import api from "../axios/axios";
+import { Button, IconButton, Alert, Snackbar } from "@mui/material";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Link, useNavigate } from "react-router-dom";
 
 function listUsers() {
-  const [users,setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [alert, setalert] = useState({
+    // Visibilidade (false = oculto; true = visivel)
+    open: false,
+
+    // Nivel do alerta (sucess, error, warning, etc)
+    severity: "",
+
+    // messagem exibida
+    message: "",
+  });
+
+  // função para exibir o alerta
+  const showAlert = (severity, message) => {
+    setalert({ open: true, severity, message });
+  };
+
+  // fechar o alerta
+  const handleCloseAlert = () => {
+    setalert({ ...alert, open: false });
+  };
+
   const navigate = useNavigate();
 
-  async function getUsers(){
+  async function getUsers() {
     // Chamada da Api
     await api.getUsers().then(
-      (response)=>{
-        console.log(response.data.users)
-        setUsers(response.data.users)
-      },(error)=>{
-        console.log("Erro ",error)
+      (response) => {
+        console.log(response.data.users);
+        setUsers(response.data.users);
+      },
+      (error) => {
+        console.log("Erro ", error);
       }
-    )
+    );
   }
 
-  const listUsers = users.map((user)=>{
-    return(
+  async function deleteUser(id) {
+    try {
+      await api.deleteUser(id);
+      await getUsers();
+      showAlert("success", "Usuário Excluido com Sucesso!");
+    } catch (error) {
+      console.log("Erro ao deletar usuario...", error);
+      showAlert("Error", error.response.data.error);
+    }
+  }
+
+  const listUsers = users.map((user) => {
+    return (
       <TableRow key={user.id_usuario}>
         <TableCell align="center">{user.name}</TableCell>
         <TableCell align="center">{user.email}</TableCell>
         <TableCell align="center">{user.cpf}</TableCell>
+        <TableCell align="center">
+          <IconButton onClick={() => deleteUser(user.id_usuario)}>
+            <DeleteIcon color="error" />
+          </IconButton>
+        </TableCell>
       </TableRow>
-    )
-  })
+    );
+  });
 
-  function logout(){
+  function logout() {
     localStorage.removeItem("authenticated");
-    navigate ("/")
+    navigate("/");
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     // if(!localStorage.getItem("authenticated")){
     //   navigate("/")
     // }
     getUsers();
-  },[]);
-
+  }, []);
 
   return (
     <div>
-      {users.length == 0 ?(<h1>Carregando Usuarios</h1>):(
-      <div>
-        <h5>Lista de usuários</h5>
-        <TableContainer component={Paper} style={{margin:"2px"}}>
-          <Table size="small">
-            <TableHead style={{backgroundColor: "brown", borderStyle:"solid"}}>
-              <TableRow>
-                <TableCell align="center">
-                  Nome
-                </TableCell>
-                <TableCell align="center">
-                  Email
-                </TableCell>
-                <TableCell align="center">
-                  CPF
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>{listUsers}</TableBody>
-          </Table>
-        </TableContainer>
-      <Button 
-      fullWidth
-      variant='contained'
-      onClick={logout}>
-        SAIR
-      </Button>
-      </div>
+      <Snackbar
+        open={alert.open}
+        autoHideDuration={3000}
+        onClose={handleCloseAlert}
+        anchorOrigin={{vertical:"top", horizontal:"center"}}
+      >
+        <Alert onClose={handleCloseAlert} severity={alert.severity}
+        sx={{
+          width:"100%"
+        }}
+        >
+          {alert.message}
+        </Alert>
+      </Snackbar>
+      {users.length == 0 ? (
+        <h1>Carregando Usuarios</h1>
+      ) : (
+        <div>
+          <h5>Lista de usuários</h5>
+          <TableContainer component={Paper} style={{ margin: "2px" }}>
+            <Table size="small">
+              <TableHead
+                style={{ backgroundColor: "purple", borderStyle: "solid" }}
+              >
+                <TableRow>
+                  <TableCell align="center">Nome</TableCell>
+                  <TableCell align="center">Email</TableCell>
+                  <TableCell align="center">CPF</TableCell>
+                  <TableCell align="center">LIXO POR AQUI</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>{listUsers}</TableBody>
+            </Table>
+          </TableContainer>
+          <Button fullWidth variant="contained" onClick={logout}>
+            SAIR
+          </Button>
+        </div>
       )}
     </div>
-  )
+  );
 }
-export default listUsers
+export default listUsers;
